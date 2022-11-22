80f6b31abf91dd24d4d47662a6f908f4942589d1
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-22 17:33:02 +0530

    Build project schema

---
+++ db/migrate/20221122103336_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :authorid
+      t.string :title
+      t.text :text
+      t.integer :commentscounter
+      t.integer :likescounter
+
+      t.timestamps
+    end
+  end
+end
