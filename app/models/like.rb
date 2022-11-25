9a0dd45d326c343d50ff4ef4296d8556aaa5406f
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-25 12:01:03 +0530

    Fix linters(rubocop)

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,11 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User'
-    after_save :update_comments_counter
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+  after_save :update_comments_counter
 
-    private
-    def update_comments_counter
-      post.increment!(:comments_counter)
-    end
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: 'author_id', dependent: :destroy
-    has_many :comments, foreign_key: 'author_id', dependent: :destroy
-    has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
-    def most_recent_3_posts
-        posts.order('created_at desc').limit(3)
-      end
+  def most_recent_3_posts
+    posts.order('created_at desc').limit(3)
+  end
 end
