631727ada5704f1088af79c5dbee1b0fbe91e299
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-26 20:24:53 +0530

    Update to add prefix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "users", to: "users#index"
-  get "users/:id", to: "users#show"
-  get "users/:user_id/posts", to: "posts#index"
-  get "users/:user_id/posts/:id", to: "posts#show"
+  get "/users", to: "users#index", as: :users
+  get "/users/:id", to: "users#show", as: :user
+  get "/users/:user_id/posts", to: "posts#index", as: :user_posts
+  get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
 
-  root to: "users#index"
+  root "users#index"
 end
