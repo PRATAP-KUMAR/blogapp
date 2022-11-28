91d04987e6a71f9cbb7946dc8e8e1ddb2ee1bc9e
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-28 12:19:41 +0530

    Refactored based on suggestion given by code-reviewer - Add action for show and edit method in routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
   def index; end
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  # resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get "/users", to: "users#index", as: :users
-  get "/users/:id", to: "users#show", as: :user
-  get "/users/:user_id/posts", to: "posts#index", as: :user_posts
-  get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
-
-  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
