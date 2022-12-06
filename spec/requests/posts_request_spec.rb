8dde9d6dcc078bbf0e2f96c3a45a59ad75f4ec50
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-06 17:33:04 +0530

    Correct linters

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'request spec testing for posts', type: :request do
+  describe '- testing for posts controller' do
+    # it 'checks for correct response status, templtae, body to match with place holder text' do
+    #   get '/users/1/posts'
+    #   expect(response.status).to eq(200)
+    #   expect(response).to render_template('posts/index')
+    #   expect(response.body).to include("This page will show a Single User's posts and show posts comments")
+    # end
+
+    # it 'checks for correct response status, templtae, body to match with place holder text' do
+    #   get '/users/1/posts/1'
+    #   expect(response.status).to eq(200)
+    #   expect(response).to render_template('posts/show')
+    #   expect(response.body).to include('This page will show Post and its comments')
+    # end
+  end
+end
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'users', type: :request do
+  describe '- testing for posts controller' do
+    # it 'checks for correct response status, templtae, body to match with place holder text' do
+    #   get '/users'
+    #   expect(response.status).to eq(200)
+    #   expect(response).to render_template('users/index')
+    #   expect(response.body).to include('This page will display list of users along with their photos')
+    # end
+
+    # it 'checks for correct response status, templtae, body to match with place holder text' do
+    #   get '/users/1'
+    #   expect(response.status).to eq(200)
+    #   expect(response).to render_template('users/show')
+    #   expect(response.body).to include('This page will display user posts and etc')
+    # end
+  end
+end
