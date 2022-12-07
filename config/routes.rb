58113e279ddb2502e5a6ec3811a002b6ed6e0061
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-07 19:57:57 +0530

    Install and setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
