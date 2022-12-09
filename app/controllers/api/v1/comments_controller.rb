d08c6ee85aff7542b72f26f89ea8479b63eaa7e6
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-09 18:00:23 +0530

    fix typo, check linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,19 +5,19 @@ class Api::V1::CommentsController < Api::V1::ApiController
     render json: @comments, status: :ok
   end
 
-  def show
-  end
+  def show; end
 
   def create
     author_id = @current_user.id,
-    text = params[:text],
-    post_id = params[:post_id]
+                text = params[:text],
+                post_id = params[:post_id]
     if author_id && text && post_id
-      comment = Comment.new(author_id:, text:, post_id:,)
+      comment = Comment.new(author_id, text, post_id)
       if comment.save
         render json: { status: 'SUCCESS', message: 'Comment Has Been Created', data: comment }, status: :ok
       else
-        render json: { status: 'ERROR', message: 'Comment Has NOT Been Created', data: comment .errors}, status: :unprocessable_entity
+        render json: { status: 'ERROR', message: 'Comment Has NOT Been Created', data: comment.errors },
+               status: :unprocessable_entity
       end
     else
       render json: { status: 'ERROR', message: 'Invalid Prameters' }, status: :not_found
